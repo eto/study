9b373bb2ebf21155015cb09f29922867860fba53
Koichiro Eto <koichiro.eto@gmail.com>
2021-12-28 18:03:31 +0100

    Update.

--- 1228/test2.rb
+++ 1228/test2.rb
@@ -9,10 +9,10 @@ SDL2::Mixer.init(SDL2::Mixer::INIT_FLAC|SDL2::Mixer::INIT_MP3|SDL2::Mixer::INIT_
 SDL2::Mixer.open(22050, SDL2::Mixer::DEFAULT_FORMAT, 2, 512)
 p SDL2::Mixer::Chunk.decoders
 p SDL2::Mixer::Music.decoders
-wave = SDL2::Mixer::Chunk.load(ARGV[0])
+file = "pi.wav"
+wave = SDL2::Mixer::Chunk.load(file)
 (0..100).each {|i|
-  #SDL2::Mixer::Channels.fade_in(0, wave, 0, 600)
-  SDL2::Mixer::Channels.fade_in(i%8, wave, 0, 600)
+  SDL2::Mixer::Channels.fade_in(i % 8, wave, 0, 600)
   sleep 0.05
 }
 while SDL2::Mixer::Channels.play?(0)
