e765aa95afa8a1b3b3337fed66bb038ff1ec26f3
Koichiro Eto <koichiro.eto@gmail.com>
2021-12-30 17:43:40 +0100

    Update.

--- 1230/test1.rb
+++ 1230/test1.rb
@@ -2,4 +2,37 @@
 # coding: utf-8
 # Copyright (C) 2004-2021 Koichiro Eto, All rights reserved.
 # License: BSD 3-Clause License
-p $0
+
+given = "6459"
+chars = given.split(//)
+nums = []
+chars.each {|char|
+  nums << char.to_i
+}
+
+operators = %w{+ - * /}
+
+expressions = []
+loop {
+  formula = []
+  formula << nums[0]
+  operators.each {|operator|
+    formula << operator
+    formula << nums[1]
+    operators.each {|operator|
+      formula << operator
+      formula << nums[2]
+      operators.each {|operator|
+        formula << operator
+        formula << nums[3]
+      }
+    }
+  }
+  formulastr = formula.join("")
+  result = eval formulastr
+  expressions[formulastr] = result
+  if result == 10
+    p formulastr
+  end
+  }
+}
