b13265a5ec27e2db66cfc4f314c0dd0a64c20da3
Koichiro Eto <koichiro.eto@gmail.com>
2022-01-05 05:38:37 +0100

    Update.

--- 0104/test4.rb
+++ 0104/test4.rb
@@ -6,7 +6,6 @@ include PyCall::Import
 
 pyimport 'cv2', as: 'cv2'
 pyimport 'mediapipe', as: 'mp'
-exit
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -14,21 +13,31 @@ mp_face_mesh = mp.solutions.face_mesh
 
 # For webcam input:
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+p drawing_spec
 cap = cv2.VideoCapture(1)
+p cap
+cap = cv2.VideoCapture.(1)
+p cap
+exit
 
 face_mesh = mp_face_mesh.FaceMesh(
     max_num_faces=1,
-    refine_landmarks=True,
+    refine_landmarks=true,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5)
 p face_mesh
 
+loop do
+  status = cap.isOpened.()
+  break unless status
+  success, image = cap.read()
+  unless success
+    print("Ignoring empty camera frame.")
+    break
+  end
+end
+
 =begin
-with mp_face_mesh.FaceMesh(
-    max_num_faces=1,
-    refine_landmarks=True,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as face_mesh:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
