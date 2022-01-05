ee0e3de870ee9c6af6f60799359e7fe7c0ce6340
Koichiro Eto <koichiro.eto@gmail.com>
2022-01-05 05:54:44 +0100

    Update.

--- 0104/test4.rb
+++ 0104/test4.rb
@@ -13,37 +13,60 @@ mp_face_mesh = mp.solutions.face_mesh
 
 # For webcam input:
 drawing_spec = mp_drawing.DrawingSpec.(thickness=1, circle_radius=1)
-#p drawing_spec
+p drawing_spec
 #cap = cv2.VideoCapture(1)
 #p cap
 cap = cv2.VideoCapture.(1)
 p cap
-exit
 
-face_mesh = mp_face_mesh.FaceMesh(
-    max_num_faces=1,
-    refine_landmarks=true,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5)
+face_mesh = mp_face_mesh.FaceMesh.({max_num_faces: 1,
+                                    refine_landmarks: true,
+                                    min_detection_confidence: 0.5,
+                                    min_tracking_confidence: 0.5})
 p face_mesh
 
-loop do
+loop {
   status = cap.isOpened.()
+  p status
   break unless status
-  success, image = cap.read()
+  success, image = cap.read.()
   unless success
     print("Ignoring empty camera frame.")
     break
   end
-end
+
+  # To improve performance, optionally mark the image as not writeable to pass by reference.
+  image.flags.writeable = false
+  image = cv2.cvtColor.(image, cv2.COLOR_BGR2RGB)
+  results = face_mesh.process.(image)
+
+  # Draw the face mesh annotations on the image.
+  image.flags.writeable = true
+  image = cv2.cvtColor.(image, cv2.COLOR_RGB2BGR)
+  if results.multi_face_landmarks
+    for face_landmarks in results.multi_face_landmarks
+      mp_drawing.draw_landmarks({image: image,
+                                 landmark_list: face_landmarks,
+                                 connections: mp_face_mesh.FACEMESH_TESSELATION,
+                                 landmark_drawing_spec: None,
+                                 connection_drawing_spec: mp_drawing_styles.get_default_face_mesh_tesselation_style()})
+      mp_drawing.draw_landmarks({image: image,
+                                 landmark_list: face_landmarks,
+                                 connections: mp_face_mesh.FACEMESH_CONTOURS,
+                                 landmark_drawing_spec: None,
+                                 connection_drawing_spec: mp_drawing_styles.get_default_face_mesh_contours_style()})
+      mp_drawing.draw_landmarks({image: image,
+                                 landmark_list: face_landmarks,
+                                 connections: mp_face_mesh.FACEMESH_IRISES,
+                                 landmark_drawing_spec: None,
+                                 connection_drawing_spec: mp_drawing_styles.get_default_face_mesh_iris_connections_style()})
+    end
+  end
+}
 
 =begin
   while cap.isOpened():
     success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
 
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
