4f8fb696ff40af303975166c9e56a345074bf13c
Koichiro Eto <koichiro.eto@gmail.com>
2022-01-17 04:24:18 +0100

    Update.

--- 0117/test1.rb
+++ 0117/test1.rb
@@ -7,16 +7,42 @@ require "pathname"
 require "qp"
 
 class Main
+  def initialize
+    @wordsfile = Pathname.new "/usr/share/dict/words"
+    #@outfile = Pathname.new "five-letters-words.txt"
+    @outfile = Pathname.new "words.txt"
+    @words = []
+  end
+
   def main(argv)
-    file = Pathname.new "/usr/share/dict/words"
-    outfile = Pathname.new "five-letters-words.txt"
-    file.open {|f|
-      outfile.open("wb") {|out|
-        while line = f.read
-          line.chomp!
-          next if line.length != 5
-          out.puts word
-        end
+    @words = get_five_letters_words(@wordsfile)
+    words = []
+    @words.each {|word|
+      next unless word.include?("s")
+      next unless word.include?("e")
+      next unless word.include?("r")
+    }
+    output_words(words, @outfile)
+  end
+
+  def get_five_letters_words(wordsfile)
+    str = wordsfile.open {|f| f.read }
+    words = []
+    str.each_line {|line|
+      #p line
+      line.chomp!
+      next if line.length != 5
+      word = line.downcase
+      #p word
+      words << word
+    }
+    return words
+  end
+
+  def output_words(words, outfile)
+    outfile.open("wb") {|out|
+      words.each {|word|
+        out.puts word
       }
     }
   end
